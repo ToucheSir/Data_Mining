42212620f036227b0941269e960a81a51484fd10
Brian Chen <touchesir@no-reply.github.com>
2019-09-28 23:21:12 -0400

    Changes to make network converge

--- mine_data.py
+++ mine_data.py
@@ -1,6 +1,7 @@
-from keras.layers import Input, Dense
-from keras.models import Model
-from keras.datasets import mnist
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Model
+from tensorflow.keras.datasets import mnist
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -19,29 +20,33 @@ class AutoEncoder:
     def import_data(self):
         path = os.getcwd()+'/x_modified.csv'
         df = pd.read_csv(path)
+        print(df.shape)
         return df
 
     def create_model(self, layer_size):
-        input_size = Input(shape=(self.df.shape[1],))
         train, test = train_test_split(self.df, test_size=0.2)
-        # inp_size = self.df.shape[1]
+        input_size = self.df.shape[1]
 
-        encoded = Dense(layer_size, activation='relu')(input_size)
-        decoded = Dense(self.df.shape[1], activation='sigmoid')(encoded)
-        self.autoencoder = Model(input_size, decoded)
-        
-        self.encoder = Model(input_size, encoded) #map input to small dimension
-        encoded_input = Input(shape=(layer_size,)) # 
-        
-        decoder_layer = self.autoencoder.layers[-1]
-        self.decoder = Model(encoded_input, decoder_layer(encoded_input))
-        self.autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+        inputs = Input(shape=(input_size,))
+        encoded = Dense(layer_size)(inputs)
+        self.encoder = Model(inputs, encoded) # map input to small dimension
+        self.encoder.summary()
 
-        #train
-        self.autoencoder.fit(train, train, epochs=200, batch_size=32, shuffle=True, validation_data=(test, test))
+        encoded_input = Input(shape=(layer_size,))
+        decoded = Dense(input_size)(encoded_input)
+        self.decoder = Model(encoded_input, decoded)
+        self.decoder.summary()
+
+        self.autoencoder = Model(inputs, self.decoder(self.encoder(inputs)))
+        self.autoencoder.summary()
+        self.autoencoder.compile(optimizer='adam', loss='mse')
+
+        # train
+        self.autoencoder.fit(train, train, epochs=500, batch_size=32, shuffle=True, validation_data=(test, test))
         encoded_imgs = self.encoder.predict(test)
         decoded_imgs = self.decoder.predict(encoded_imgs)
         print(decoded_imgs[:1,:10])
 
 
-ae = AutoEncoder()
+if __name__ == "__main__":
+    ae = AutoEncoder()
